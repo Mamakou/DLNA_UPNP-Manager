8619b20f9a63df2f51866b325b21606bb2b8d6fb
Mamakou <814889621@qq.com>
2018-12-17 15:05:50 +0800

    Initial Commit

---
+++ DLNA/DLNA.h
@@ -0,0 +1,19 @@
+//
+//  DLNA.h
+//  DLNA
+//
+//  Created by goviewtech on 2018/12/17.
+//  Copyright © 2018 goviewtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLNA.
+FOUNDATION_EXPORT double DLNAVersionNumber;
+
+//! Project version string for DLNA.
+FOUNDATION_EXPORT const unsigned char DLNAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLNA/PublicHeader.h>
+
+
